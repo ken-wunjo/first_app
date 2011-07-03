61b86ca4e567887002cc9cf1dc3f41699e54303e
ken ernst <kernst@mobileposse.com>
2011-07-03 09:53:26 -0400

    init commit - first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0571be2652191285e6ec3f12831ce54b1077b773072d9d01b7db311ed58bc9f5d76008a50eecada91958c0ff5354e790b89f48bdc863890b591967d38394ba31'
